f00fb59b82fab3e2733fff539494ec08eb11485c
jeffry56 <c.gianfranco56@gmail.com>
2021-04-21 21:17:55 +0200

    fix05

---
+++ credentials.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID "Telecom-24350745"                        // your ssid
+#define WIFI_PASSWD "1Q1nTZOQ0BWpcOk7yIPyvohy"              // WLAN AP password"
+#define WIFI_SSID1 "AndroidAP"
+#define WIFI_PASSWD1 "a28b3288fdc6"
+#define AP_SSID "MalignaniAP"
+#define AP_PASSWD "malignani"
