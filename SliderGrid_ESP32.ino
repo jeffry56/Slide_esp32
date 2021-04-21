b6133eb5203322a853604b676820afca1c103508
jeffry56 <c.gianfranco56@gmail.com>
2021-04-21 17:15:11 +0200

    first

---
+++ SliderGrid_ESP32.ino
@@ -0,0 +1,387 @@
+/***** documentation AsyncWebServer on https://github.com/me-no-dev/ESPAsyncWebServer **************************/
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+#include "index.h"
+#include "credentials.h"
+
+#define porta           80
+#define socketPort      81
+#define HOSTNAME        "MyESP-"
+
+
+const char* ssid        = WIFI_SSID;
+const char* password    = WIFI_PASSWD;
+const char* ssid1       = WIFI_SSID1;
+const char* password1   = WIFI_PASSWD1;
+const char* APssid      = AP_SSID;
+const char* APpassword  = AP_PASSWD;
+
+/*const char* ssid      = "Telecom-24350745";
+  const char* password  = "1Q1nTZOQ0BWpcOk7yIPyvohy";
+  const char* ssid1     = "AndroidAP";
+  const char* password1 = "a28b3288fdc6";
+  const char* APssid    = "MalignaniAP";
+  const char* APpassword = "malignani";*/
+
+IPAddress ip(192, 168, 1, 51);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(8, 8, 8, 8);
+IPAddress secondaryDNS(8, 8, 4, 4);
+IPAddress myIP;
+
+int         n;
+int         maxfail       = 0;
+const int   maxattemp     = 30;                     // assegnare maxattemp >=15 per effettuare la connessione ad una rete WiFi
+
+bool ledState = 0;
+const int ledPin = 2;
+
+int actualPosition  = 0;
+int maxPosition     = 1000;
+int minPosition     = 0;
+int A_position      = 0;
+int B_position      = 0;
+int Interval        = 0;
+int Shots           = 0;
+int ShotsMade       = 0;
+bool Achk, Bchk = false;
+
+WiFiMulti wifiMulti;
+AsyncWebServer server(porta);
+AsyncWebSocket ws("/ws");
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  if (!WiFi.config(ip, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("STA Failed to configure");
+  }
+
+  start_SPIFFS();
+  startWiFi();
+  initWebSocket();
+  startServer();
+}
+
+void loop() {
+  ws.cleanupClients();
+  digitalWrite(ledPin, ledState);
+}
+
+/*** Functions Definitions ****************************************************************/
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+  Serial.println("\nWebSocket server started.");
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      send_values(0);
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    String command = (char*)data;
+    Serial.println(command);
+    if (command.substring(0, 4) == "V13-") {
+      Shots = (command.substring(4)).toInt();
+      send_values(0);
+    } else {
+      if (command.substring(0, 4) == "V14-") {
+        Interval = (command.substring(4)).toInt();
+        send_values(0);
+      } else {
+        int nCommand = (command.substring(3)).toInt();
+        Serial.println(nCommand);
+        if (nCommand > 0) {
+          switch (nCommand) {
+            case 1:                                               // Increment position value by 1
+              send_values(1);
+              break;
+            case 2:                                               // Increment position value by 10
+              send_values(10);
+              break;
+            case 3:                                               // Increment position value by 100
+              send_values(100);
+              break;
+            case 4:                                               // Decrement position value by 1
+              send_values(-1);
+              break;
+            case 5:                                               // Decrement position value by 10
+              send_values(-10);
+              break;
+            case 6:                                               // Decrement position value by 100
+              send_values(-100);
+              break;
+            case 7:                                               // Set (A) value
+              A_position = actualPosition;
+              send_values(0);
+              break;
+            case 8:                                               // (A) jog button - see case 20 and 21
+              break;
+            case 9:                                               // (A) switch - see case 40 and 41
+              send_values(0);
+              break;
+            case 10:                                              // Set (B) value
+              B_position = actualPosition;
+              send_values(0);
+              break;
+            case 11:                                              // (B) jog button - see case 30 and 31
+              break;
+            case 12:                                              // (A) switch - see case 50 and 51
+              send_values(0);
+              break;
+            case 13:                                              // Shots input
+              send_values(0);
+              break;
+            case 14:                                              // Interval input
+              Interval = 14;
+              send_values(0);
+              break;
+            case 20:                                              // (A) jog button mouse down event
+              Serial.println("down08");
+              break;
+            case 21:                                              // (A) jog button mouse up event
+              Serial.println("up08");
+              break;
+            case 30:                                              // (B) jog button mouse down event
+              Serial.println("down11");
+              break;
+            case 31:                                              // (B) jog button mouse up event
+              Serial.println("up11");
+              break;
+            case 40:                                              // (A) switch on
+              Serial.println("chk09");
+              Achk = true;
+              send_values(0);
+              break;
+            case 41:                                              // (A) switch off
+              Serial.println("unchk09");
+              Achk = false;
+              send_values(0);
+              break;
+            case 50:                                              // (B) switch on
+              Serial.println("chk12");
+              Bchk = true;
+              send_values(0);
+              break;
+            case 51:                                              // (B) switch off
+              Serial.println("unchk12");
+              Bchk = false;
+              send_values(0);
+              break;
+            default:
+              send_values(0);
+              break;
+          }
+        }
+      }
+    }
+  }
+}
+
+void send_values(int incdec) {
+  actualPosition = actualPosition + incdec;
+  if (actualPosition > maxPosition) {
+    actualPosition = maxPosition;
+  }
+  if (actualPosition < minPosition) {
+    actualPosition = minPosition;
+  }
+  String msg = "dati|" + String(A_position) + "|" + String(B_position) + "|" + String(actualPosition) + "|" + String(Achk) + "|" + String(Bchk) + "|" + String(Shots) + "|" + String(Interval) + "|" + String(ShotsMade);
+  ws.textAll(msg);
+}
+
+String processor(const String& var) {             // Replaces placeholder with values
+  Serial.println(var);
+  if (var == "actualPosition") {
+    return String(actualPosition);
+  }
+  if (var == "A_position") {
+    return String(A_position);
+  }
+  if (var == "B_position") {
+    return String(B_position);
+  }
+  if (var == "Achk") {
+    if (Achk) {
+      return String("checked");
+    } else {
+      return String("");
+    }
+  }
+  if (var == "Bchk") {
+    if (Bchk) {
+      return String("checked");
+    } else {
+      return String("");
+    }
+  }
+  if (var == "Shots") {
+    return String(Shots);
+  }
+  if (var == "Interval") {
+    return String(Interval);
+  }
+  if (var == "ShotsMade") {
+    return String(ShotsMade);
+  }
+}
+
+void startServer() {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/html", webpage, processor);
+  });
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+  server.on("/iconic_fill.css", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/iconic_fill.css", "text/css");
+  });
+  server.on("/bg_01.jpg", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/bg_01.jpg", "image/jpeg");
+  });
+  server.on("/icomoon.woff", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/icomoon.woff", "font/woff");
+  });
+  server.on("/icomoon.ttf", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/icomoon.ttf", "font/ttf");
+  });
+  server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/favicon.ico", "image/ico");
+  });
+
+  server.onNotFound([](AsyncWebServerRequest * request) {
+    Serial.printf("NOT_FOUND: ");
+    if (request->method() == HTTP_GET) Serial.printf("GET");
+    else if (request->method() == HTTP_POST) Serial.printf("POST");
+    else if (request->method() == HTTP_DELETE) Serial.printf("DELETE");
+    else if (request->method() == HTTP_PUT) Serial.printf("PUT");
+    else if (request->method() == HTTP_PATCH) Serial.printf("PATCH");
+    else if (request->method() == HTTP_HEAD) Serial.printf("HEAD");
+    else if (request->method() == HTTP_OPTIONS) Serial.printf("OPTIONS");
+    else Serial.printf("UNKNOWN");
+    Serial.printf(" http://%s%s\n", request->host().c_str(), request->url().c_str());
+
+    if (request->contentLength()) {
+      Serial.printf("_CONTENT_TYPE: %s\n", request->contentType().c_str());
+      Serial.printf("_CONTENT_LENGTH: %u\n", request->contentLength());
+    }
+    int headers = request->headers();
+    int i;
+    for (i = 0; i < headers; i++) {
+      AsyncWebHeader* h = request->getHeader(i);
+      Serial.printf("_HEADER[%s]: %s\n", h->name().c_str(), h->value().c_str());
+    }
+    int params = request->params();
+    for (i = 0; i < params; i++) {
+      AsyncWebParameter* p = request->getParam(i);
+      if (p->isFile()) {
+        Serial.printf("_FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
+      } else if (p->isPost()) {
+        Serial.printf("_POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+      } else {
+        Serial.printf("_GET[%s]: %s\n", p->name().c_str(), p->value().c_str());
+      }
+    }
+    request->send(404);
+  });
+
+  server.begin();                                      // Start web server
+  Serial.println("\nTCP (HTTP) server started.");
+}
+
+void start_SPIFFS() {
+  if (SPIFFS.begin(true)) {
+    Serial.println("\nSPIFFS Active");
+    Serial.println();
+  } else {
+    Serial.println("Unable to activate SPIFFS");
+  }
+}
+
+void startWiFi() {
+  WiFi.persistent(false);                 // Avoid wear-out
+  WiFi.mode(WIFI_STA);
+  //  WiFi.mode(WIFI_OFF);
+  //  WiFi.forceSleepBegin();
+  WiFi.config(ip, gateway, subnet, primaryDNS);
+
+  if (WiFi.status() != WL_CONNECTED) {
+    wifiMulti.addAP(ssid, password);
+    wifiMulti.addAP(ssid1, password1);
+  }
+
+  Serial.println("Connecting...");
+
+  //  ESP.restart();
+
+  while ((wifiMulti.run() != WL_CONNECTED) && (maxfail < maxattemp)) {
+    Serial.print(".");
+    maxfail += 1;
+    delay(500);
+  }
+
+  if (maxfail < maxattemp) {
+    Serial.print("\nConnected: ");
+    Serial.print("SSID: "); Serial.print(WiFi.SSID());
+    Serial.print(", IP: "); myIP = WiFi.localIP(); Serial.print(myIP);
+    Serial.print(":"); Serial.print(porta);
+  }
+  else {
+    Serial.println("\nNo Connected ");
+    Serial.print("Config access point - ");
+
+    WiFi.mode(WIFI_AP);
+    //WiFi.softAP(const char* ssid, const char* password, int channel, int ssid_hidden, int max_connection)
+    WiFi.softAP(APssid, APpassword);
+    WiFi.softAPConfig(ip, gateway, subnet);
+
+    myIP = WiFi.softAPIP();
+    Serial.print(" IP: "); Serial.print(myIP);
+  }
+
+  esp_chip_info_t chip_info;
+  esp_chip_info(&chip_info);
+
+  Serial.println("\n\nHardware info:");
+  Serial.printf("\t%d cores Wifi %s%s\n", chip_info.cores, (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "", (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+  Serial.printf("\tSilicon revision: %d\n", chip_info.revision);
+  Serial.printf("\t%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024), (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embeded" : "external");
+
+  String chipId = String((uint32_t)ESP.getEfuseMac(), HEX);
+  chipId.toUpperCase();
+
+  String hostname(HOSTNAME);
+  hostname += chipId;
+  WiFi.hostname(hostname);
+
+  Serial.printf("\tChip id: %s\n", chipId.c_str());
+  Serial.println();
+  //Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s:%d\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str(), porta);
+  Serial.print("Hostname: "); Serial.println(hostname);
+}
---
+++ index.h
@@ -0,0 +1,260 @@
+const char webpage[] = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Focus Bracketing App</title>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" type="text/css" href="style.css" >
+      <link rel="stylesheet" type="text/css" href="iconic_fill.css" >
+   </head>
+   <body>
+      <div class="page-container">
+         <h1 class="page-title">
+            Focus Bracketing
+         </h1>
+         <div class="external-grid">
+            <div>
+               <div class="panel-header" style= "background-color:rgb(241, 240, 178);">Controls position</div>
+               <div class="panel-grid">
+                  <div class="grid-item">
+                     <!--<div class="grid-item-title">Step 100</div>-->
+                     <div class="buttons">
+                        <input id="Cmd01" class="buttons-input" type="button" onclick="decodKey(this)">
+                        <label for="Cmd01" class="buttons-label1"> <i class="icon-arrow-right-1"></i></label>
+                     </div>
+                   </div>
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Step 10</div>-->
+                     <div class="buttons">
+                        <input id="Cmd02" class="buttons-input" type="button" onclick="decodKey(this)">
+                        <label for="Cmd02" class="buttons-label1"> <i class="icon-double-arrow-right-1"></i></label>
+                     </div>
+                  </div>
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Step 1</div>-->
+                     <div class="buttons">
+                        <input id="Cmd03" class="buttons-input" type="button" onclick="decodKey(this)">
+                        <label for="Cmd03" class="buttons-label1"> <i class="icon-triple-arrow-right-1"></i></label>
+                     </div>
+                   </div>
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Step 1</div>-->
+                     <div class="buttons case">
+                        <input id="Cmd04" class="buttons-input" type="button" onclick="decodKey(this)">
+                        <label for="Cmd04" class="buttons-label1"> <i class="icon-arrow-left-1"></i></label>
+                     </div>
+                  </div>
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Step 10</div>-->
+                     <div class="buttons case">
+                        <input id="Cmd05" class="buttons-input" type="button" onclick="decodKey(this)">
+                        <label for="Cmd05" class="buttons-label1"> <i class="icon-double-arrow-left-1"></i></label>
+                     </div>
+                  </div>
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Step 100</div>-->
+                     <div class="buttons case">
+                        <input id="Cmd06" class="buttons-input" type="button" onclick="decodKey(this)">
+                        <label for="Cmd06" class="buttons-label1"> <i class="icon-triple-arrow-left-1"></i></label>
+                     </div>
+                  </div>
+               </div>
+               <div class="panel-footer" style= "background-color:rgb(241, 240, 178);">
+               </div>
+            </div>
+               <!--********************************************************************************************************************-->
+            <div>
+               <div class="panel-header" style= "background-color:rgb(178, 241, 178);">Point A</div>
+               <div class="panel-2-grid">
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Set</div>-->
+                     <div class="buttons">
+                        <input id="Cmd07" class="buttons-input" type="button" onclick="decodKey(this)">
+                        <label for="Cmd07" class="buttons-label"> <i class="icon-cog"></i></label>
+                     </div>
+                  </div>
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Jog</div>-->
+                     <div class="buttons">
+                        <input id="Cmd08" class="buttons-input" type="button">
+                        <label for="Cmd08" class="buttons-label" id="Jog08"> <i class="icon-arrow-right"></i></label>
+                     </div>
+                  </div>
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Go/Stop</div>-->
+                     <div class="buttons">
+                        <input id="Cmd09" class="buttons-input" type="checkbox" %Achk% onclick="decodKey(this)">
+                        <label for="Cmd09" class="buttons-label" id="Label09"> <i class="icon-switch"></i></label>
+                     </div>
+                  </div>
+               </div>
+               <div class="panel-footer" style= "background-color:rgb(178, 241, 178);">
+                  <div class="text">Home point value A =</div>
+                  <div class="value"><span id="A_position">%A_position%</span></div>
+               </div>
+            </div>
+<!--***********************************************************************************************************************************-->
+            <div>
+               <div class="panel-header" style= "background-color:rgb(253, 171, 171);">Point B</div>
+               <div class="panel-2-grid">
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Set</div>-->
+                     <div class="buttons case">
+                        <input id="Cmd10" class="buttons-input" type="button" onclick="decodKey(this)">
+                        <label for="Cmd10" class="buttons-label"> <i class="icon-cog"></i></label>
+                     </div>
+                  </div>
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Jog</div>-->
+                     <div class="buttons case">
+                        <input id="Cmd11" class="buttons-input" type="button">
+                        <label for="Cmd11" class="buttons-label" id="Jog11"> <i class="icon-arrow-left"></i></label>
+                     </div>
+                  </div>
+                  <div class="grid-item">
+                     <!--<div <div class="grid-item-title">Go/Stop</div>-->
+                     <div class="buttons case">
+                        <input id="Cmd12" class="buttons-input" type="checkbox" %Bchk% onclick="decodKey(this)">
+                        <label for="Cmd12" class="buttons-label" id="Label12"> <i class="icon-switch"></i></label>
+                     </div>
+                  </div>
+               </div>
+               <div class="panel-footer" style= "background-color:rgb(253, 171, 171);">
+                  <div class="text">Home point value B =</div>
+                  <div class="value"><span id="B_position">%B_position%</span></div>
+               </div>
+            </div>
+<!--**********************************************************************************************************************************-->
+         <div>
+            <div class="panel-header" style= "background-color:rgb(182, 248, 250);">Input values</div>
+            <div class="panel-1-grid">
+               <label for="Cmd13"><b>Shots set </b>[n] =</label>
+               <input type="number" id="Cmd13" name="shots-set" value=%Shots% onchange="sendValue(this)">
+               <div class="text"><b>Actual position </b>[mm] =</div>
+               <div class="value"> <span id="actualPosition">%actualPosition%</span></div>
+            </div>
+            <div class="panel-1-grid">
+               <label for="Cmd14"><b>Interval </b>[sec] =</label>
+               <input type="number" id="Cmd14" name="interval" value=%Interval% onchange="sendValue(this)">
+               <div class="text"><b>Shots made </b>[n] =</div>
+               <div class="value"> <span id="ShotsMade">%ShotsMade%</span></div>
+            </div>
+         <div class="panel-footer" style= "background-color:rgb(182, 248, 250);">
+         </div>
+          </div>
+     </div>
+      <script>
+        var gateway = `ws://${window.location.hostname}/ws`;
+        var websocket;
+        window.addEventListener('load', onLoad);
+
+        document.getElementById("Jog08").onmousedown = function() {mouseDown(this)};
+        document.getElementById("Jog11").onmousedown = function() {mouseDown(this)};
+        document.getElementById("Jog08").ontouchstart = function() {mouseDown(this)};
+        document.getElementById("Jog11").ontouchstart = function() {mouseDown(this)};
+        document.getElementById("Jog08").onmouseup = function() {mouseUp(this)};
+        document.getElementById("Jog11").onmouseup = function() {mouseUp(this)};
+        document.getElementById("Jog08").ontouchend = function() {mouseUp(this)};
+        document.getElementById("Jog11").ontouchend = function() {mouseUp(this)};
+        
+        function mouseDown(ctrl) {
+            var isChecked1 = document.getElementById("Cmd09").checked;
+            var isChecked2 = document.getElementById("Cmd12").checked;
+            if (isChecked1 || isChecked2) {return}
+            if (ctrl.id==="Jog08") {websocket.send("Jog20");}
+            if (ctrl.id==="Jog11") {websocket.send("Jog30");}
+          }
+          
+         function mouseUp(ctrl) {
+            var isChecked1 = document.getElementById("Cmd09").checked;
+            var isChecked2 = document.getElementById("Cmd12").checked;
+            if (isChecked1 || isChecked2) {return}
+            if (ctrl.id==="Jog08") {websocket.send("Jog21");}
+            if (ctrl.id==="Jog11") {websocket.send("Jog31");}
+          }
+
+         function decodKey(ctrl) {
+            var isChecked1 = document.getElementById("Cmd09").checked;
+            var isChecked2 = document.getElementById("Cmd12").checked;
+            if (isChecked1) { websocket.send("Chk40"); } else { websocket.send("Chk41"); }
+            if (isChecked2) { websocket.send("Chk50"); } else { websocket.send("Chk51"); }
+            SetResetSwitch();
+            if (isChecked1 || isChecked2) {
+               return
+            }
+             send_cmd_id(ctrl);
+         }
+         
+         function SetResetSwitch() {
+            var isChecked1 = document.getElementById("Cmd09").checked;
+            var isChecked2 = document.getElementById("Cmd12").checked;
+            if (isChecked1) {
+                document.getElementById("Cmd12").disabled = true; 
+               document.getElementById("Label12").style.opacity = "0.3";
+            } else {
+               document.getElementById("Cmd12").disabled = false;
+               document.getElementById("Label12").style.opacity = "1" ;
+               }
+            if (isChecked2) {
+               document.getElementById("Cmd09").disabled = true; 
+               document.getElementById("Label09").style.opacity = "0.3";
+            } else {
+               document.getElementById("Cmd09").disabled = false;
+               document.getElementById("Label09").style.opacity = "1" ;
+               }
+        }
+
+          function sendValue(ctrl) {
+            var shots = document.getElementById("Cmd13");
+            var Interval = document.getElementById("Cmd14");
+            websocket.send("V13-"+shots.value);
+            websocket.send("V14-"+Interval.value);
+          }
+
+          function onLoad(event) {
+            initWebSocket();
+          }
+          
+          function initWebSocket() {
+            /*console.log('Trying to open a WebSocket connection...');*/
+            websocket = new WebSocket(gateway);
+            websocket.onopen    = onOpen;
+            websocket.onclose   = onClose;
+            websocket.onmessage = onMessage;
+          }
+          
+          function onOpen(event) {
+            console.log('Connection opened');
+          }
+          
+          function onClose(event) {
+            console.log('Connection closed');
+            setTimeout(initWebSocket, 2000);
+          }
+          
+          function onMessage(event) {
+            console.log(`Received a notification from ${event.origin}`);
+            console.log(event);
+            var str = event.data.split('|');
+            if (str[0]==="dati") {
+              document.getElementById("A_position").innerHTML = str[1];
+              document.getElementById("B_position").innerHTML = str[2];
+              document.getElementById("actualPosition").innerHTML = str[3];
+              document.getElementById("Cmd09").checked = (str[4]=="1");
+              document.getElementById("Cmd12").checked = (str[5]=="1");
+              document.getElementById("Cmd13").value = str[6];
+              document.getElementById("Cmd14").value = str[7];
+              document.getElementById("ShotsMade").innerHTML = str[8];
+              SetResetSwitch();
+            }
+          }
+                
+          function send_cmd_id(ctrl){
+            websocket.send(ctrl.id);
+            /*console.log(ctrl);*/
+          }     
+     </script>
+   </body>
+</html>
+)=====";
