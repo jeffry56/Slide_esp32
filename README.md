c9b6796433b1ead40c866d6d6d709c930dcfa5dd
jeffry56 <c.gianfranco56@gmail.com>
2021-04-21 17:31:48 +0200

    credentials

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Slide_esp32
+
+# Before start
+
+```
+mv credential-default.h credentials.h
+```
---
+++ credentials-default.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID ""                        // your ssid
+#define WIFI_PASSWD ""              // WLAN AP password"
+#define WIFI_SSID1 ""
+#define WIFI_PASSWD1 ""
+#define AP_SSID ""
+#define AP_PASSWD ""
